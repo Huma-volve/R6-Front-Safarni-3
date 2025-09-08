c26f8020ab4e0f9c76fbce1272f2981f19a196d2
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2025-09-08 19:54:45 +0300

    chore: add required libraries for project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
 
 function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <h1 className="">hello</h1>
-      <Button>Click me</Button>
-    </div>
-  );
+    return (
+        <div className="flex min-h-svh flex-col items-center justify-center">
+            <h1 className="">hello</h1>
+            <Button>Click me</Button>
+            <Search className="text-red-600" />
+        </div>
+    );
 }
 
 export default App;
