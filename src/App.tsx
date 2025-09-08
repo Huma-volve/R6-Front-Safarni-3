2b03cef7cf3e469b58b7e06653caf53e47005916
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2025-09-08 20:39:22 +0300

    chore: finalize project file structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Search } from "lucide-react";
 function App() {
     return (
         <div className="flex min-h-svh flex-col items-center justify-center">
-            <h1 className="">hello</h1>
             <Button>Click me</Button>
             <Search className="text-red-600" />
         </div>
